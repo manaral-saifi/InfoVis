54caf7eb60bd9b845180560e75886dbbb9421f82
Pascal Lindemann <38719784+lip16661@users.noreply.github.com>
2018-05-16 03:09:58 +0200

    Test-Map erstellt mit OpenLayers!

---
+++ Soccer Map.js
@@ -0,0 +1,12 @@
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([12.297383, 49.669448]),
+      zoom: 15
+    })
+  });
