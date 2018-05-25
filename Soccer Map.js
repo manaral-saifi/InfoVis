c788cc3e714cf73588cbf0c0fb8526c9f9678f71
Pascal Lindemann <38719784+lip16661@users.noreply.github.com>
2018-05-25 10:19:29 +0200

    Top 6000 transfers gescrapt von transfermarkt.de !

--- Soccer Map.js
+++ Soccer Map.js
@@ -1,12 +1,19 @@
 var d3 = d3 || {};
 
-const globalURI = "./data/spi_global_rankings.csv";
+const globalURI = "./data/transfers_test.csv";
 
 /*Only works in firefox so far*/
 
 function parseToList(csvURI){
     d3.csv(csvURI, function(data){
-        console.log(data);
+        var list = [];
+        for(let i = 0; i < data.length; i++){
+            list.push({"player":null,"from":"null","to":null});
+            list[i].player = data[i].player.substring(0, data[i].player.indexOf("(")-1);
+            list[i].from = data[i].from.substring(0, data[i].from.indexOf("(")-1);
+            list[i].to = data[i].to.substring(0, data[i].to.indexOf("(")-1);
+        }
+        console.log(list);
     });
 }
 
