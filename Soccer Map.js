05640041b678111624a52eec300484a994a8f1a5
Pascal Lindemann <38719784+lip16661@users.noreply.github.com>
2018-06-20 15:12:17 +0200

    Erster Zentrums-Marker gesetzt, Zentrum (Erstplatzierter) wird fokussiert !

--- Soccer Map.js
+++ Soccer Map.js
@@ -1,6 +1,7 @@
 var d3 = d3 || {};
 
 const globalURI = "./data/transfers_test.csv";
+const rankingURI = "./data/spi_global_rankings.csv";
   var lon, 
       lat,
       test = "test",
@@ -15,15 +16,34 @@ function parseToList(csvURI){
             list[i].from = data[i].from.substring(0, data[i].from.indexOf("(")-1);
             list[i].to = data[i].to.substring(0, data[i].to.indexOf("(")-1);
         }
-        console.log(list);
     });
 }
 
+function parseRankingsToList(csvURI){
+    d3.csv(csvURI, function(data){
+        var list = [];
+        for(let i = 0; i < data.length; i++){
+            list.push({"name":null,"league":null});
+            list[i].name = data[i].name;
+            list[i].league = data[i].league;
+        }
+        createMap(list[0]);
+        for(let i = 0; i < 50; i++){
+            
+        }
+        console.log(list);
+    })
+}
+
 parseToList(globalURI);
+parseRankingsToList(rankingURI);
 var geocoder;
 
-function createMap() {
-    map = new ol.Map({
+function createMap(centrum) {
+    var centerAddress = centrum.name + " stadium " + centrum.league;
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': centerAddress}, function(results, status){
+        map = new ol.Map({
         target: 'map',
         layers: [
             new ol.layer.Tile({
@@ -31,21 +51,38 @@ function createMap() {
             })
         ],
         view: new ol.View({
-            center: ol.proj.fromLonLat([0, 0]),
-            zoom: 15
+            center: ol.proj.fromLonLat([results[0].geometry.viewport.b.f, results[0].geometry.viewport.f.f]),
+            zoom: 4
         })
+    });
+        var pos = ol.proj.fromLonLat([results[0].geometry.viewport.b.f, results[0].geometry.viewport.f.f]);
+
+      // center marker
+      var marker = new ol.Overlay({
+            position: pos,
+            positioning: 'center-center',
+            element: document.getElementById('marker'),
+            stopEvent: false
+      });
+      map.addOverlay(marker);
+
+      // Bayern label
+      var bayern = new ol.Overlay({
+        position: pos,
+        element: document.getElementById('bayern')
+      });
+      map.addOverlay(bayern);
     });
 }
 
 function codeAddress() {
-    geocoder = new google.maps.Geocoder();
     var address = document.getElementById('address').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
         if (status == 'OK') {
             lat = results[0].geometry.viewport.f.f;
             lon = results[0].geometry.viewport.b.f;
             
-            map.getView().setCenter(ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857'));            
+            //map.getView().setCenter(ol.proj.transform([lon, lat], 'EPSG:4326', 'EPSG:3857'));            
         
             var marker = new google.maps.Marker({
                 position: results[0].geometry.location
@@ -55,7 +92,5 @@ function codeAddress() {
         }
     });
 }
-
-createMap();
 
 console.log(test);
