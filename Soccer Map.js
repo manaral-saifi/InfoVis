11df86e0fa2594462ea25e0f31ba18da02081d67
Manar Al-Saifi <manar.al-saifi@stud.uni-regensburg.de>
2018-06-20 12:24:02 +0200

    - Geocoder eingebunden - Test ist erfolgreich - Bug in der Darstellung

--- Soccer Map.js
+++ Soccer Map.js
@@ -1,7 +1,9 @@
 var d3 = d3 || {};
 
 const globalURI = "./data/transfers_test.csv";
-
+  var lan;
+    var lat;
+var test = "test";
 /*Only works in firefox so far*/
 
 function parseToList(csvURI){
@@ -19,15 +21,34 @@ function parseToList(csvURI){
 
 parseToList(globalURI);
 
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([12.297383, 49.669448]),
-      zoom: 15
-    })
-  });
+ var geocoder;
+
+
+  function codeAddress() {
+        geocoder = new google.maps.Geocoder();
+    var address = document.getElementById('address').value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+          lat = results[0].geometry.viewport.f.f;
+          lon = results[0].geometry.viewport.b.f;
+          var map = new ol.Map({
+                target: 'map',
+                layers: [
+                    new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                    })
+                ],
+                view: new ol.View({
+                    center: ol.proj.fromLonLat([lon, lat]),
+                    zoom: 15
+                })
+        });
+        var marker = new google.maps.Marker({
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+console.log(test);
